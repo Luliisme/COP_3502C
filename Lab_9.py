d9276ebf56fafdab75aab390f2b60de2e6966732
Luliisme <166638142+Luliisme@users.noreply.github.com>
2024-04-10 13:10:37 -0400

    Lab_9.py(2)
    Before partner code

--- Lab_9.py
+++ Lab_9.py
@@ -1,4 +1,5 @@
 # Lauren Lessne
+
 def menu():
 
     print("""
@@ -7,26 +8,39 @@ Menu
 1. Encode
 2. Decode
 3. Quit""")
+    pass
 
-def encode():
+def encode(password):
+    list = [i for i in password]
+    list = (map(int, list))
+    encode = [x+3 for x in list]
+    encodeFINAL = ''.join(str(i) for i in encode)
+    return encodeFINAL
 
+def decode(encoded):
+# Partner Code
 
 
 if __name__ == '__main__':
 
+    menu()
 
     while True:
-        option = input("Please enter an option: ")
+        option = input("\nPlease enter an option: ")
 
-        if option == 1:
+        if option == "1":
             password = input("Please enter your password to encode: ")
             print("Your password has been encoded and stored!")
 
-        if option == 2:
-            print("The encoded password is", encode(password) + ", and the original password is" + password)
+        if option == "2":
+            print("The encoded password is " + encode(password) +", and the original password is", password +".")
 
-        if option == 3:
+        if option == "3":
             break
+
+
+
+
 
 
 
